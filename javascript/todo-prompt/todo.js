4f0f3e6abd8fd327ecb42096cd426613350a61e6
Nicole Sattler <nicole.sattler@live.com>
2017-06-19 18:51:21 +0000

    Added delete function to console todo list

--- javascript/todo-prompt/todo.js
+++ javascript/todo-prompt/todo.js
@@ -1,17 +1,41 @@
 var todos = ['Learn to code'];
 
+console.log("\nWelcome to consoleTodos!");
+
 var input = prompt('What would you like to do?');
 
 while (input !== "quit") {
     if (input === "list") {
-        console.log(todos);
+        listTodos();
     }
     else if (input === "new") {
-        var newTodo = prompt('Enter new todo: ');
-        todos.push(newTodo);
+        addTodo();
     }
-    
+    else if (input === "delete") {
+        deleteTodo();
+    }
+
     input = prompt('What would you like to do?');
 }
 
-console.log('You quit the app. Goodbye!')
+console.log('\nYou quit the app. Goodbye!')
+
+function addTodo() {
+    var newTodo = prompt('Enter new todo: ');
+    todos.push(newTodo);
+    console.log('\nTodo added!');
+}
+
+function deleteTodo() {
+    var index = prompt("Enter index number of todo to delete: ");
+    todos.splice(index, 1);
+    console.log('\nTodo deleted!');
+}
+
+function listTodos() {
+    console.log("\n**********");
+    todos.forEach(function(todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("**********");
+}
