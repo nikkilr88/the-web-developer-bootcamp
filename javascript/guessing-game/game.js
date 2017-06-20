aa6aa6512eb182437fb2034435d58efe9037f314
Nicole Sattler <nicole.sattler@live.com>
2017-06-20 21:23:03 +0000

    Modified number guessing game to work with while loop

--- javascript/guessing-game/game.js
+++ javascript/guessing-game/game.js
@@ -1,10 +1,15 @@
-var num = 4;
+var num = Math.floor(Math.random() * 10);
+
 var guess = Number(prompt("Guess a number: "));
 
-if(guess === num) {
-    alert('You got it!');
-} else if(guess > num){
-    alert('Too high! Try again!');
-} else {
-    alert('Too low! Try again!');
+while (guess !== num) {
+    if (guess > num) {
+        alert('Too high! Try again!');
+    }
+    else {
+        alert('Too low! Try again!');
+    }
+    guess = Number(prompt("Guess a number: "));
 }
+
+alert('You got it!');
