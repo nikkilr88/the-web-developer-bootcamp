a098dfea8463a83009e2aa48449e30dc00db5936
Nicole Sattler <nicole.sattler@live.com>
2017-07-03 22:28:13 +0000

    Added jQuery selectors exercise

---
+++ jquery/selectors/selectors.js
@@ -0,0 +1,6 @@
+/* global $ */
+
+$('div').css('background', 'purple');
+$('.highlight').css('width', '200px');
+$('#third').css('border', '2px solid orange');
+$('div').first().css('color', 'pink');
