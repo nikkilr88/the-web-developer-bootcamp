186314373f429d2df5ef15a0854be06199304391
Nicole Sattler <nicole.sattler@live.com>
2017-07-10 00:06:50 +0000

    Added 'add new todo' function

--- jquery/todo/script.js
+++ jquery/todo/script.js
@@ -1,12 +1,23 @@
 /* global $ */
 
 //Check off todos by clicking
-
-$('li').on('click', function() {
+$('ul').on('click', 'li', function() {
     $(this).toggleClass('completed');
 });
 
-$('span').on('click', function(e) {
+//Delete todos
+$('ul').on('click', 'span', function(e) {
     e.stopPropagation();
-    $(this).parent().remove();
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+});
+
+//Add new todo
+$('input[type="text"]').on('keypress', function(e){
+    if(e.which === 13) {
+        var todoText = $(this).val();
+        $(this).val('');
+        $('ul').append('<li><span>X </span>'+todoText+'</li>')
+    }
 });
