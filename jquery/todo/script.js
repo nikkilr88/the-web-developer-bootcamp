10def74f8ede9df3eebbc72ce677ec90f7a92d1a
Nicole Sattler <nicole.sattler@live.com>
2017-07-10 21:41:26 +0000

    Changed icon for dynamically added todos

--- jquery/todo/script.js
+++ jquery/todo/script.js
@@ -18,6 +18,6 @@ $('input[type="text"]').on('keypress', function(e){
     if(e.which === 13) {
         var todoText = $(this).val();
         $(this).val('');
-        $('ul').append('<li><span>X </span>'+todoText+'</li>')
+        $('ul').append('<li><span><i class="fa fa-trash"></i> </span>'+todoText+'</li>')
     }
 });
